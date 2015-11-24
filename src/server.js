0872d3921731b675af799a01df0ddc2ff8301d89
Evgeny Matveev <masaki.tux@gmail.com>
2015-11-24 14:18:21 +0300

    Broadcasting State from A Redux Listener

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  store.subscribe(
+    () => io.emmit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emmit('state', store.getState().toJS());
+  });
 }
